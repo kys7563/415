b70fe9482e6773c8718a9c2be17107f1290b24bb
kys7563 <kys7563@kaist.ac.kr>
2022-03-11 15:55:50 +0900

    hotfix

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -14,7 +14,7 @@
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
-/*merge the world!!*/
+/*hotfix the world!!*/
 
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
